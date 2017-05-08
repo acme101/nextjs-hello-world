1fb93ecbc616a3ab8b538472e4433d5f57df8468
hoatle <hoatle@teracy.com>
2017-05-08 12:35:19 +0700

    deploy helm

--- scripts/deploy_gcp.sh
+++ scripts/deploy_gcp.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-gcloud auth activate-service-account --key-file /tmp/gcp_key_file.json
+gcloud auth activate-service-account $GCP_ACCOUNT --key-file /tmp/gcp_key_file.json
 #ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 
 gcloud docker -a
@@ -24,4 +24,8 @@ gcloud container clusters get-credentials $GCP_CLUSTER_NAME
 #kubectl create secret docker-registry registry.gitlab.com --docker-server=$CI_REGISTRY --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
 kubectl version
 
-kubectl apply -f k8s/deployment.yaml
+#kubectl apply -f k8s/deployment.yaml
+
+helm version
+
+helm upgrade $HELM_RELEASE_NAME --set image.repository=$HELM_IMAGE_REPOSITORY,image.tag=$HELM_IMAGE_TAG,image.pullPolicy=$HELM_IMAGE_PULL_POLICY,image.lastDeployed=$HELM_IMAGE_LAST_DEPLOYED --install helm-charts/nextjs-hello-world
